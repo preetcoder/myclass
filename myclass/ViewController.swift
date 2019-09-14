4c8e5fa0c6cdb6dff5ce907745d9b4aad91aaf30
Harpreet <s3701661@student.rmit.edu.au>
2019-09-14 14:30:40 +1000

    Initial Commit

---
+++ myclass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myclass
+//
+//  Created by Harpreet Singh on 14/9/19.
+//  Copyright © 2019 Harpreetandbhavesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
