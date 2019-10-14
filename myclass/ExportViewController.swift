cdd016206a48913a75e4a6043b9c2593d6263a38
Harpreet <s3701661@student.rmit.edu.au>
2019-10-15 00:44:06 +1100

    Assessment Marks Export Finished

--- myclass/ExportViewController.swift
+++ myclass/ExportViewController.swift
@@ -16,6 +16,8 @@ class ExportViewController: UIViewController, MFMailComposeViewControllerDelegat
     var studentmanager = StudentManager()
     
     var attendancemanager = AttendanceManager()
+    
+    var markmanager = MarkManager()
   
     @IBAction func exportAssessments(_ sender: Any) {
         
@@ -38,11 +40,10 @@ class ExportViewController: UIViewController, MFMailComposeViewControllerDelegat
         }
     }
     
+    // method with string parameter of attendance or assessment to export
     func getAllStudentAttendance(val : String) -> String{
         
         var allStudentData = ""
-        
-        print(val)
         
         if(val == "attendance") {
             
@@ -75,23 +76,27 @@ class ExportViewController: UIViewController, MFMailComposeViewControllerDelegat
         }
         else{
             
-            let obj = ConvertToDate()
+            //let obj = ConvertToDate()
             let  allStudents = studentmanager.getStudentsfromDB()
             
             for student in allStudents{
                 
-//                if (student.getMarks()?.count)! > 0 {
-//                    allStudentData += "Student ID - \(student.getStudentID())  "
-//                    
-//                    
-//                    for assessmentMarks in student.getMarks()! {
-//                        
-//                         allStudentData += "\n Assessment Name - \(assessmentMarks.getAssessment().getAssessmentTitle()) \n Marks Obtained = \(assessmentMarks.getObtainedMarks())"
-//                        
-//                    }
-//                    
-//                    allStudentData += "\n ================ \n"
-//                }
+                
+                if ((student.marks?.count)!) > 0 {
+                    allStudentData += "Student ID - \(student.getStudentID)"
+                    
+                    
+                    for case let assessmentMarks as Mark in student.marks! {
+                        
+                        // get assessment details from Mark entity object
+                         allStudentData += "\n Assessment Name - \(assessmentMarks.getAssessment.getAssessmentTitle) \n Marks Obtained = \(assessmentMarks.getObtainedMarks)"
+                      
+                       
+
+
+                    }
+                    allStudentData += "\n ================ \n"
+                }
                 
             }
         }
@@ -104,9 +109,12 @@ class ExportViewController: UIViewController, MFMailComposeViewControllerDelegat
     func configureMailComposer(val : String) -> MFMailComposeViewController{
         let mailComposeVC = MFMailComposeViewController()
         mailComposeVC.mailComposeDelegate = self
-        mailComposeVC.setToRecipients(["s3701661@student.rmit.edu.au","s3695018@student.rmit.ed.au"])
+       
+        // set mail parameters
+    mailComposeVC.setToRecipients(["s3701661@student.rmit.edu.au","s3695018@student.rmit.ed.au"])
         mailComposeVC.setSubject("File Export")
         mailComposeVC.setMessageBody(getAllStudentAttendance(val: val), isHTML: false)
+        
         return mailComposeVC
     }
     
@@ -119,20 +127,6 @@ class ExportViewController: UIViewController, MFMailComposeViewControllerDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
-       
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
